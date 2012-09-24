e74b5aef7c0039a0607fc3809a21e7014e3b6440
tripples <sanketsudake@gmail.com>
2012-09-24 23:08:55 +0530

    Check was removed

--- source/output.sh
+++
@@ -1,7 +0,0 @@
-# /******************************************/
-# /* Author:-Sanket Sudake                  */
-# /* Licence:- GNU GPL v3                   */
-# /* Project Name:-Machine Operating System */
-# /* ****************************************/
-#!/bin/bash
-cat /home/valtzar/MOS/output.txt
